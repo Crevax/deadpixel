4d00472ce24d4227585220e9d493caa205c34159
CJ Davis <cj@cjdavis.me>
2014-05-26 19:23:05 -0400

    Base Rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeadPixel::Application.config.secret_key_base = '6c8b16cb67c695874c5c5a36ea815c2e8bd037c537f373f873847bfb5490fe7812ec0e1bf93644353bcf19b308a11118154121a414febe887dcc6ce644237dc4'
