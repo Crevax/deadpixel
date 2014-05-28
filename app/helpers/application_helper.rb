fafa6e07a1d78b31a91ec60a87544a90c41c6049
CJ Davis <cj@cjdavis.me>
2014-05-28 15:19:21 -0400

    Remove 'Dead Pixel: ' from playlist titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,21 @@ module ApplicationHelper
 
     # Returns the full title on a per-page basis.
     def full_title(page_title)
-        base_title = 'Dead Pixel'
+      base_title = 'Dead Pixel'
         if page_title.empty?
-            base_title
+          base_title
         else
-            "#{base_title} - #{page_title}"
+          "#{base_title} - #{page_title}"
         end
+    end
+
+    # Returns a playlist title sans Dead Pixel:
+    def playlist_title(title)
+      if title.empty?
+        "Unnamed"
+      else
+        title.slice! "Dead Pixel: "
+        title
+      end
     end
 end
