eb3ec9b76b84477f5be4c96579a40a06918c1569
CJ Davis <cj@cjdavis.me>
2014-05-27 20:21:58 -0400

    Add basic user feed to homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
 
   def home
-
+    $client = YouTubeIt::Client.new(:dev_key=> "")
+    @videos = $client.videos_by(:user => "DeadPixelShow", :max_results => 6)
   end
 end
